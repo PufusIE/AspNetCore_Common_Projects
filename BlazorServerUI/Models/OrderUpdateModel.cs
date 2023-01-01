e37914b794bf7e0ea0d603b4f177889ebd04df02
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2023-01-01 18:51:50 +0000

    Blazor Server UI CRUD Actions.

---
+++ BlazorServerUI/Models/OrderUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerUI.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("New Order Name")]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters.")]
+        [MinLength(3, ErrorMessage = "You need to exnter at least 3 characters for an order name.")]
+        public string UpdatedName { get; set; }
+    }
+}
